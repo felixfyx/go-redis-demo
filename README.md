320838da503fe6eb3883fba36909cd99f3fa7c14
FelixF <felixfyx@gmail.com>
2023-08-03 18:25:47 +0800

    Working pub/sub model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-redis-demo
 An attempt at working with go and redis
+
+In this demo, the server will be the publisher and the client will be the subscriber. Whena server push out a single message, every client should receive said message.
+
+Following tutorial from here: https://dev.to/franciscomendes10866/using-redis-pub-sub-with-golang-mf9
--- redis-server/main.go
+++ redis-server/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 )
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var redisClient = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+})
 
 func main() {
 	fmt.Println("Running server...")
+	app := fiber.New()
+	var ctx = context.Background()
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		user := new(User)
+
+		if err := c.BodyParser(user); err != nil {
+			panic(err)
+		}
+
+		payload, err := json.Marshal(user)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := redisClient.Publish(ctx, "send-user-data", payload).Err(); err != nil {
+			panic(err)
+		}
+
+		return c.SendStatus(200)
+	})
+
+	app.Listen(":3000")
 }
